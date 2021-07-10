50337e781c97967be89f8057cc05f461e631f109
Govardhan-github <govardhanbajjuri1998@gmail.com>
2021-07-09 21:04:43 -0700

    UMPWAGE ADDED

--- helloworld.java
+++ helloworld.java
@@ -2,6 +2,9 @@ class HelloWorld  {
 		public static void main(String[] args) {
 
 		System.out.println("hello world");
+int wagePerHour = 20;
+int fullDayHour = 8;
+int dailyWage = 0;
 
 int IS_FULL_TIME = 1;
 		double empCheck = Math.floor(Math.random() * 10) % 2;
